e43e6468f861483b81083992de081b974fb3911a
zhaorutcd <zhaoru@tcd.ie>
2022-11-16 15:52:52 +0000

    latimes parser class, test code in app.java

---
+++ src/main/java/ie/tcd/app.java
@@ -0,0 +1,118 @@
+package ie.tcd;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.BooleanClause;
+import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+import ie.tcd.docParser.*;
+
+public class app {
+
+	private static String INDEX_DIRECTORY = "../index";
+	
+	// Limit the number of search results we get
+    private static int MAX_RESULTS = 10;
+
+
+    public static void main(String[] args) throws IOException {
+
+        try {
+        	// create documents use latimes 
+			ArrayList<Document> la_docs = latimes_parser.loadLaTimesDocs("D:\\tcd\\CS7IS3_Information_Retrieval_and_Web_Search\\assignment2\\Assignment Two_Documents\\Assignment_Two\\latimes");
+		
+			create_index(la_docs);
+			
+			do_query();
+			
+        } catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+    }
+    private static void create_index(ArrayList<Document> docs) {
+    	 try {
+           	 // Analyzer that is used to process TextField
+                Analyzer analyzer = new StandardAnalyzer();
+
+                // Open the directory that contains the search index
+                Directory directory = FSDirectory.open(Paths.get(INDEX_DIRECTORY));
+             
+                // Set up an index writer to add process and save documents to the index
+                IndexWriterConfig config = new IndexWriterConfig(analyzer);
+                config.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+                IndexWriter iwriter = new IndexWriter(directory, config);
+
+   		     // Write all the documents in the linked list to the search index
+                iwriter.addDocuments(docs);
+                
+             // Commit everything and close
+                iwriter.close();
+                directory.close();
+                System.out.println("Index Created at: " + System.getProperty("user.dir"));
+                
+            }
+            catch (Exception e) {
+
+            }
+    }
+    
+    private static void do_query(){
+    	try {
+	        // Open the folder that contains our search index
+	        Directory directory = FSDirectory.open(Paths.get(INDEX_DIRECTORY));
+	
+	        // create objects to read and search across the index
+	        DirectoryReader ireader = DirectoryReader.open(directory);
+	        IndexSearcher isearcher = new IndexSearcher(ireader);
+	
+	        // builder class for creating our query
+	        BooleanQuery.Builder query = new BooleanQuery.Builder();
+	
+	        // Some words that we want to find and the field in which we expect
+	        // to find them
+	        Query term1 = new TermQuery(new Term("text", "Gorbachev"));
+	        Query term2 = new TermQuery(new Term("text", "glasnost"));
+//	        Query term3 = new TermQuery(new Term("content", "criticism"));
+	
+	        // construct our query using basic boolean operations.
+	        query.add(new BooleanClause(term1, BooleanClause.Occur.SHOULD   ));   // AND
+	        query.add(new BooleanClause(term2, BooleanClause.Occur.SHOULD));     
+//	        query.add(new BooleanClause(term3, BooleanClause.Occur.MUST_NOT)); // NOT
+	
+	        // Get the set of results from the searcher
+	        ScoreDoc[] hits = isearcher.search(query.build(), MAX_RESULTS).scoreDocs;
+	
+	        // Print the results
+	        System.out.println("Documents: " + hits.length);
+	        for (int i = 0; i < hits.length; i++)
+	        {
+	                Document hitDoc = isearcher.doc(hits[i].doc);
+	                System.out.println(i + ") " + hitDoc.get("docno") + " " + hits[i].score);
+	        }
+	
+	        // Close everything we used
+	        ireader.close();
+	        directory.close();
+    	}
+    	catch (Exception e) {
+
+        }
+    }
+}
---
+++ src/main/java/ie/tcd/docParser/latimes_parser.java
@@ -0,0 +1,54 @@
+package ie.tcd.docParser;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class latimes_parser {
+
+    public static ArrayList<Document> loadLaTimesDocs(String pathToLADocs) throws IOException {
+        System.out.println("Loading LATIMES ...");
+        ArrayList<Document> laDocs = new ArrayList<>();
+
+        File folder = new File(pathToLADocs);
+        File[] allFiles = folder.listFiles();
+
+        for (File file : allFiles) {
+
+            org.jsoup.nodes.Document allContent = Jsoup.parse(file, null, "");
+            System.out.println("reading file: " + file.getName());
+            Elements docs = allContent.select("DOC");
+
+            for (Element doc : docs) {
+                String docNo, date, headline, text;
+                docNo = (doc.select("DOCNO").text());
+                date = (doc.select("Date").select("P").text());
+                headline = (doc.select("HEADLINE").select("P").text());
+                text = (doc.select("TEXT").select("P").text());
+                laDocs.add(createDocument(docNo, date, headline, text));
+            }
+        }
+        System.out.println("Loading LATIMES Done!");
+        return laDocs;
+    }
+
+    private static org.apache.lucene.document.Document createDocument(String docNo, String date, String headline, String text) {
+        org.apache.lucene.document.Document document = new org.apache.lucene.document.Document();
+        document.add(new StringField("docno", docNo, Field.Store.YES));
+        document.add(new TextField("date", date, Field.Store.YES));
+        document.add(new TextField("headline", headline, Field.Store.YES));
+        document.add(new TextField("text", text, Field.Store.YES));
+        return document;
+    }
+
+  
+}
